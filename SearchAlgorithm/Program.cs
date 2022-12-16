a157add39d81239c115da2f7c96196317cbc050e
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-16 14:18:07 +0700

    get the number of elements to store in the array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,8 +8,21 @@ namespace SearchAlgorithm
         int[] arr = new int[20];
         //Number of elements in the array 
         int n;
-        //Get thr number of elements to store in the array
+        //Get the number of elements to store in the array
         int i;
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
