e41ab3416560cd4661859daf4dfc5bb4b095aa0d
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-16 14:27:24 +0700

    obtain the index of the elements in the array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -45,6 +45,9 @@ namespace SearchAlgorithm
                 //Apply binary search
                 int lowerbound = 0;
                 int upperbound = n - 1;
+                //obtain the index of the elements in the array
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
             }
         }
         static void Main(string[] args)
