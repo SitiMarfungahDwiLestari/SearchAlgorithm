f5ce262f0ef5276aeb0236f313cdb9aef1e1ade1
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-16 14:22:06 +0700

    accept array elements

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -22,6 +22,17 @@ namespace SearchAlgorithm
                 else
                     Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
             }
+            //Accept array elements 
+            Console.WriteLine("");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("--------------------");
+            for (i = 0; i<n; i ++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
         static void Main(string[] args)
         {
